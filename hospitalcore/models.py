2a5b2d6b1c9b157c641dde2c494a6830c884f0b5
Mohammed Sadiq Ali <sadikcp2014@gmail.com>
2024-12-17 12:50:54 +0530

    Add new fields and models for user health status and medical history

--- hospitalcore/models.py
+++ hospitalcore/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class UserModel(models.Model):
     user_id = models.CharField(max_length=20)
+    password = models.CharField(max_length=100)
     username = models.CharField(max_length=150)
     email = models.EmailField()
     phone = models.CharField(max_length=13)
@@ -15,9 +16,45 @@ class UserModel(models.Model):
 class DoctorModel(models.Model):
     doc_id = models.CharField(max_length=20)
     doc_name = models.CharField(max_length=250)
+    category = models.CharField(max_length=250)
     phone = models.CharField(max_length=13)
+
+    def __str__(self):
+        return self.doc_id
+    
 
 class MedicalHistory(models.Model):
     user_id = models.OneToOneField(UserModel,on_delete=models.CASCADE)
+    condition = models.CharField(max_length=1500)
+    severity = models.CharField(max_length=1500)
+    medication = models.CharField(max_length=1500)
     date = models.DateField(auto_now=True)
-    doctor = models
+    doctor = models.ManyToManyField(DoctorModel)
+    status = models.CharField(max_length=150)
+    remark = models.CharField(max_length=200)
+    outcome = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.user_id
+    
+class HeathStatus(models.Model):
+    user_id = models.OneToOneField(UserModel,on_delete=models.CASCADE)
+    pressure = models.CharField(max_length=100)
+    sugar = models.CharField(max_length=100)
+    cholestrol = models.CharField(max_length=100)
+    pre_expl = models.CharField(max_length=300)
+    sug_expl = models.CharField(max_length=300)
+    chol_expl = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.user_id
+    
+class MedNews(models.Model):
+    headline = models.CharField(max_length=1500)
+    news = models.CharField(max_length=1500)
+    date = models.DateField(auto_now=True)
+    doctor = models.ManyToManyField(DoctorModel)
+    image = models.CharField(max_length=1500)
+
+    def __str__(self):
+        return self.headline

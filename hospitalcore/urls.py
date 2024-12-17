6c7fd04d1ed7b7e3a2e24ba8a9c96c2b1671ce2b
Mohammed Sadiq Ali <sadikcp2014@gmail.com>
2024-12-17 15:27:06 +0530

    Add hospitalcore app and configure URLs; implement views for User, Doctor, Medical History, Heath Status, Med News, and Appointment

---
+++ hospitalcore/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("create-user", views.UserList.as_view(), name="create-user"),
+    path("user/<int:pk>", views.UserDetail.as_view(), name="user-detail"),
+    path("list-users", views.ListUser.as_view(), name="list-users"),
+    path("create-doctor", views.DoctorList.as_view(), name="create-doctor"),
+    path("doctor/<int:pk>", views.DoctorDetail.as_view(), name="doctor-detail"),
+    path("create-med-history", views.MedHistoryList.as_view(), name="create-med-history"),
+    path("med-history/<int:pk>", views.MedHistoryDetail.as_view(), name="med-history-detail"),
+    path("create-heath-status", views.HeathStatusList.as_view(), name="create-heath-status"),
+    path("heath-status/<int:pk>", views.HeathStatusDetail.as_view(), name="heath-status-detail"),
+    path("create-med-news", views.MedNewsList.as_view(), name="create-med-news"),
+    path("med-news/<int:pk>", views.MedNewsDetail.as_view(), name="med-news-detail"),
+    path("create-appointment", views.CreateAppointment.as_view(), name="list-med-news"),
+    path("list-appointment", views.ListAppointment.as_view(), name="create-appointment"),
+]
--- hospitalcore/views.py
+++ hospitalcore/views.py
@@ -1,3 +1,89 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import UserModel, DoctorModel, HeathStatus, MedNews, Appointment, MedRecord, Prescription, MedicalHistory
+from .serializers import UserSerializer, DoctorSerializer, HeathStatusSerializer, MedNewsSerializer, AppointmentSerializer, MedRecordSerializer, PrescriptionSerializer, MedHistorySerializer
 # Create your views here.
+
+
+# User Views
+class UserList(generics.ListCreateAPIView):
+    queryset = UserModel.objects.all()
+    serializer_class = UserSerializer
+
+class ListUser(generics.ListAPIView):
+    queryset = UserModel.objects.all()
+    serializer_class = UserSerializer   
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = UserModel.objects.all()
+    serializer_class = UserSerializer
+
+class CreateUser(generics.CreateAPIView):
+    queryset = UserModel.objects.all()
+    serializer_class = UserSerializer
+
+# Doctor Views
+class DoctorList(generics.ListCreateAPIView):
+    queryset = DoctorModel.objects.all()
+    serializer_class = DoctorSerializer
+
+class DoctorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = DoctorModel.objects.all()
+    serializer_class = DoctorSerializer
+
+class CreateDoctor(generics.CreateAPIView):
+    queryset = DoctorModel.objects.all()
+    serializer_class = DoctorSerializer
+
+# Medical History Views
+class MedHistoryList(generics.ListCreateAPIView):
+    queryset = MedicalHistory.objects.all()
+    serializer_class = MedHistorySerializer
+
+class MedHistoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MedicalHistory.objects.all()
+    serializer_class = MedHistorySerializer
+
+class CreateMedHistory(generics.CreateAPIView):
+    queryset = MedicalHistory.objects.all()
+    serializer_class = MedHistorySerializer
+
+# Heath Status Views
+class HeathStatusList(generics.ListCreateAPIView):
+    queryset = HeathStatus.objects.all()
+    serializer_class = HeathStatusSerializer
+
+class HeathStatusDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = HeathStatus.objects.all()
+    serializer_class = HeathStatusSerializer
+
+class CreateHeathStatus(generics.CreateAPIView):
+    queryset = HeathStatus.objects.all()
+    serializer_class = HeathStatusSerializer
+
+# Med News Views
+class MedNewsList(generics.ListCreateAPIView):
+    queryset = MedNews.objects.all()
+    serializer_class = MedNewsSerializer
+
+class MedNewsDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MedNews.objects.all()
+    serializer_class = MedNewsSerializer
+
+class CreateMedNews(generics.CreateAPIView):
+    queryset = MedNews.objects.all()
+    serializer_class = MedNewsSerializer
+
+# Appointment Views
+class ListAppointment(generics.ListCreateAPIView):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+
+class DetailAppointment(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+
+class CreateAppointment(generics.CreateAPIView):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+
